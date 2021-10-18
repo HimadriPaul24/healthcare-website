212101f4d6265ee2a1b2fee2e0240767c9394239
HimadriPaul24 <himadripaul821@gmail.com>
2021-10-19 05:27:26 +0600

    1st commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUd8Hh59A_I8tSL98dIkBuUtM8xNTqGao",
+    authDomain: "medical-center-website.firebaseapp.com",
+    projectId: "medical-center-website",
+    storageBucket: "medical-center-website.appspot.com",
+    messagingSenderId: "248370927193",
+    appId: "1:248370927193:web:ea3d76103f8de172584348"
+};
+  
+export default firebaseConfig;
